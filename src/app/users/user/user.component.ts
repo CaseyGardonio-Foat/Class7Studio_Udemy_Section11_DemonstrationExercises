8c1753e5bfb1ba639fb943f29466ce6920db7301
Casey <casey.gardonio@foat.org>
2020-12-20 17:06:40 -0600

    completed through 11-135

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router'
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -15,13 +17,20 @@ export class UserComponent implements OnInit {
     this.user = {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
-    }
-    this.route.params.subscribe(
+    };
+    this.paramsSubscription = this.route.params.subscribe(
       (params: Params) => {
         this.user.id = params['id'];
         this.user.name = params['name']
       }
-    )
+    );
   }
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
+  }
+  /*this, and the accompanying implementation of OnDestroy, allows unsubscribe from params when an instance
+  of this component is destroyed; 
+  note that this also requires the subscription to be asigned to a new variable; this variable is
+  not necessary if you won't be using the ngOnDestroy() method*/
 
 }
