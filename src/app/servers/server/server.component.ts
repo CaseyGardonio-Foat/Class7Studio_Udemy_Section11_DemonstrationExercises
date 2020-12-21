2e98abdb87163c0282e3e226f42839e4ed9cc12f
Casey <casey.gardonio@foat.org>
2020-12-21 11:48:58 -0600

    completed through 11-141

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -24,7 +24,7 @@ export class ServerComponent implements OnInit {
   }
 
   onEditServer() {
-    this.router.navigate(['edit'], {relativeTo: this.route})
+    this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling: 'preserve'})
   }
 
 }
