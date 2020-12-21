88ed0ce2ae6eb6ba44d78bb2f448cc1f41d9511b
Casey <casey.gardonio@foat.org>
2020-12-21 14:13:50 -0600

    completed through 11-147

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from "@angular/core";
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from "@angular/router";
+import { CanActivate, CanActivateChild, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from "@angular/router";
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from './auth.service'
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
     constructor(private authService: AuthService, private router: Router) {};
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): 
     Observable<boolean> | Promise<boolean> | boolean {
@@ -19,5 +19,10 @@ export class AuthGuard implements CanActivate {
                         }
                     }
             );
+    }
+    
+    canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+    Observable<boolean> | Promise<boolean> | boolean {
+        return this.canActivate(route, state);
     }
 }
